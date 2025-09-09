25c2293883c6756e22e91e81a74b3691172842b9
Heloise18 <heloisezelma18@gmail.com>
2025-09-09 11:41:25 -0300

    fixing usecase

--- Endpoints/AuthEndpoint.cs
+++ Endpoints/AuthEndpoint.cs
@@ -8,6 +8,14 @@ public static class AuthEndpoints
 {
     public static void ConfigureAuthEndpoints(this WebApplication app)
     {
-        
+        // app.MapPost("login", async(
+        //     httpContext http,
+        //     [FromBody] AuthRequest request,
+        //     [FromService] AuthUseCase usecase => 
+        // {
+            
+        // }
+
+        // ));
     }
 }
--- UseCases/EditList/EditListUseCase.cs
+++ UseCases/EditList/EditListUseCase.cs
@@ -3,21 +3,20 @@ using simulado.Services.JWT;
 
 namespace simulado.UseCases.EditList;
 
-public class EditListUseCase(FicsDbContext ctx, IJWTService jwt)
+public class EditListUseCase(FicsDbContext ctx)
 {
     public async Task<Result<EditListResponse>> Do(EditListRequest request)
     {
         var readList = await ctx.ReadLists
             .FirstOrDefaultAsync(rl => rl.ID == request.ListID);
 
-        if (ReadList.OwnerID != request.OwnerData.ID)
+        if (readList is null)
+            return Result<EditListResponse>.Fail("ReadList não encontrada!");
+
+        if (readList.OwnerID != request.OwnerData.ID)
             return Result<EditListResponse>.Fail("Acesso negado!");
 
-        if (ReadList is null)
-            return  Result<EditListResponse>.Fail("ReadList não encontrada!");
-
-
-        var fic = readList.Fanfics.GetByID(request.FicId);
+        var fic = await ctx.Fanfics.FirstOrDefaultAsync(f => f.ID == request.FicId);
         readList.Fanfics.Add(fic);
 
         await ctx.SaveChangesAsync();
